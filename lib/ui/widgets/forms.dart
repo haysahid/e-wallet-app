b9794be769f7c04d940e22bb97217269db26e0c0
Sahid Anwar <hay.sahid@gmail.com>
2023-08-01 22:15:13 +0700

    initial commit

---
+++ lib/ui/widgets/forms.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/theme.dart';
+
+class CustomFormField extends StatelessWidget {
+  const CustomFormField({
+    super.key,
+    required this.title,
+    this.obscureText = false,
+    this.controller,
+  });
+
+  final String title;
+  final bool obscureText;
+  final TextEditingController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: blackTextStyle.copyWith(
+            fontWeight: medium,
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        TextFormField(
+          obscureText: obscureText,
+          controller: controller,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(14),
+            ),
+            contentPadding: EdgeInsets.all(12),
+          ),
+        ),
+      ],
+    );
+  }
+}
