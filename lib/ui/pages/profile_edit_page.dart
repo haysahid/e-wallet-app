a93aaa8181b91194711e915317c077de92c82378
Sahid Anwar <hay.sahid@gmail.com>
2023-08-04 16:37:20 +0700

    feat: consume api profile flow

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,11 +1,38 @@
+import 'package:bank_sha/blocs/auth/auth_bloc.dart';
+import 'package:bank_sha/models/user_edit_form_model.dart';
+import 'package:bank_sha/shared/shared_methods.dart';
 import 'package:flutter/material.dart';
 
 import '../../shared/theme.dart';
 import '../widgets/buttons.dart';
 import '../widgets/forms.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProfileEditPage extends StatelessWidget {
+class ProfileEditPage extends StatefulWidget {
   const ProfileEditPage({super.key});
+
+  @override
+  State<ProfileEditPage> createState() => _ProfileEditPageState();
+}
+
+class _ProfileEditPageState extends State<ProfileEditPage> {
+  final usernameController = TextEditingController(text: '');
+  final nameController = TextEditingController(text: '');
+  final emailController = TextEditingController(text: '');
+  final passwordController = TextEditingController(text: '');
+
+  @override
+  void initState() {
+    super.initState();
+
+    final authState = context.read<AuthBloc>().state;
+    if (authState is AuthSuccess) {
+      usernameController.text = authState.user.username!;
+      nameController.text = authState.user.name!;
+      emailController.text = authState.user.email!;
+      passwordController.text = authState.user.password!;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,68 +40,100 @@ class ProfileEditPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Edit Profile'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            padding: const EdgeInsets.all(22),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: whiteColor,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailed) {
+            showCustomSnackbar(context, state.e);
+          }
+
+          if (state is AuthSuccess) {
+            Navigator.pushNamedAndRemoveUntil(
+                context, '/profile-edit-success', (route) => false);
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // NOTE: USERNAME INPUT
-                const CustomFormField(
-                  title: 'Username',
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: const EdgeInsets.all(22),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: whiteColor,
                 ),
-                const SizedBox(
-                  height: 16,
-                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // NOTE: USERNAME INPUT
+                    CustomFormField(
+                      title: 'Username',
+                      controller: usernameController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
 
-                // NOTE: NAME INPUT
-                const CustomFormField(
-                  title: 'Full Name',
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
+                    // NOTE: NAME INPUT
+                    CustomFormField(
+                      title: 'Full Name',
+                      controller: nameController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
 
-                // NOTE: EMAIL INPUT
-                const CustomFormField(
-                  title: 'Email Address',
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
+                    // NOTE: EMAIL INPUT
+                    CustomFormField(
+                      title: 'Email Address',
+                      controller: emailController,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
 
-                // NOTE: PASSWORD INPUT
-                const CustomFormField(
-                  title: 'Password',
-                  obscureText: true,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
+                    // NOTE: PASSWORD INPUT
+                    CustomFormField(
+                      title: 'Password',
+                      obscureText: true,
+                      controller: passwordController,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
 
-                // Button
-                CustomFilledButton(
-                  title: 'Update Now',
-                  onPressed: () {
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, '/profile-edit-success', (route) => false);
-                  },
+                    // Button
+                    CustomFilledButton(
+                      title: 'Update Now',
+                      onPressed: () {
+                        context.read<AuthBloc>().add(
+                              AuthUpdateUser(
+                                UserEditFormModel(
+                                  username: usernameController.text,
+                                  name: nameController.text,
+                                  email: emailController.text,
+                                  password: passwordController.text,
+                                ),
+                              ),
+                            );
+                      },
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
--- lib/ui/pages/profile_edit_pin_page.dart
+++ lib/ui/pages/profile_edit_pin_page.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../blocs/auth/auth_bloc.dart';
+import '../../shared/shared_methods.dart';
 import '../../shared/theme.dart';
 import '../widgets/buttons.dart';
 import '../widgets/forms.dart';
 
-class ProfileEditPinPage extends StatelessWidget {
+class ProfileEditPinPage extends StatefulWidget {
   const ProfileEditPinPage({super.key});
+
+  @override
+  State<ProfileEditPinPage> createState() => _ProfileEditPinPageState();
+}
+
+class _ProfileEditPinPageState extends State<ProfileEditPinPage> {
+  final oldPinController = TextEditingController(text: '');
+  final newPinController = TextEditingController(text: '');
 
   @override
   Widget build(BuildContext context) {
@@ -13,46 +24,74 @@ class ProfileEditPinPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Edit PIN'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        children: [
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            padding: const EdgeInsets.all(22),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: whiteColor,
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailed) {
+            showCustomSnackbar(context, state.e);
+          }
+
+          if (state is AuthSuccess) {
+            Navigator.pushNamedAndRemoveUntil(
+                context, '/profile-edit-success', (route) => false);
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CustomFormField(
-                  title: 'Old PIN',
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                padding: const EdgeInsets.all(22),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: whiteColor,
                 ),
-                const SizedBox(
-                  height: 16,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CustomFormField(
+                      title: 'Old PIN',
+                      controller: oldPinController,
+                      obscureText: true,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    CustomFormField(
+                      title: 'New PIN',
+                      controller: newPinController,
+                      obscureText: true,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    CustomFilledButton(
+                      title: 'Update Now',
+                      onPressed: () {
+                        context.read<AuthBloc>().add(
+                              AuthUpdatePin(
+                                oldPinController.text,
+                                newPinController.text,
+                              ),
+                            );
+                      },
+                    ),
+                  ],
                 ),
-                const CustomFormField(
-                  title: 'New PIN',
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                CustomFilledButton(
-                  title: 'Update Now',
-                  onPressed: () {
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, '/profile-edit-success', (route) => false);
-                  },
-                ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
